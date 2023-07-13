61faacc2c1211e7c48b0c699a4baee692d56c27e
jaimin <jm690660@dal.ca>
2023-07-13 01:32:21 -0300

    Feature-1 & Feature-2 done.

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -4,26 +4,8 @@ import './App.css';
 import Home from './Pages/Home';
 import Profile from './Pages/Profile';
 import Verification from './Pages/Verification';
-import { Amplify } from 'aws-amplify';
-import { useEffect } from 'react';
 
 function App() {
-
-  useEffect(() => {
-    // Amplify.configure({
-    //   Auth: {
-    //     region: 'us-east-1',
-    //     userPoolId: 'us-east-1_1Xcd4lxHQ',
-    //     userPoolWebClientId: '6hn9vmanqlt905sa1n0skc8ql6',
-    //     oauth: {
-    //       domain: 'triviatitans.auth.us-east-1.amazoncognito.com',
-    //       scope: ['email', 'profile', 'openid'],
-    //       redirectSignIn: 'http://localhost:3000',
-    //       responseType: 'code'
-    //     }
-    //   }
-    // });
-  }, [])
 
   return (
     <BrowserRouter>
