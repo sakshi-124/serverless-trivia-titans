e09381f0688952d9bf4ed0b81dea9025e21b2723
Sakshi Vaidya <vaidyasakshi434@gmail.com>
2023-07-23 11:05:16 -0300

    instant messaging done

--- Front-end/src/Components/Msg.js
+++ Front-end/src/Components/Msg.js
@@ -1,122 +1,148 @@
-  import AddIcon from "@mui/icons-material/Add";
-  import MinimizeIcon from "@mui/icons-material/Minimize";
-  import SendIcon from "@mui/icons-material/Send";
-  import React, { useEffect, useState } from "react";
-  import ChatBot from "../Assets/robot.png";
-  import "../Components/ChatBot/ChatBotWidget.css"
-  import io from 'socket.io-client'
-  
-  const ENDPOINT  = "http://localhost:5000";
-  var socket , selectedChatCompare; 
-  
-  const Msg = () => {
-    const [messages, setMessages] = useState([]);
-    const [inputText, setInputText] = useState("");
-    const [isExpanded, setIsExpanded] = useState(false);
-    const [isConnected , setConnected] = useState(false);
- 
-    const userData = localStorage.getItem('user');
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import AddIcon from "@mui/icons-material/Add";
+import MinimizeIcon from "@mui/icons-material/Minimize";
+import SendIcon from "@mui/icons-material/Send";
 
-  
-    const sendMessage = async (event) => {
-      event.preventDefault();
-  
-      if (inputText.trim() === "") {
-        return;
-      }
-  
+const ENDPOINT = 'http://localhost:5000';
+let socket;
+
+const Msg = () => {
+  const [messages, setMessages] = useState([]);
+  const [inputText, setInputText] = useState('');
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isConnected, setConnected] = useState(false);
+  const [userList, setUserList] = useState([]);
+
+  const selectedChat = {
+    _id: 1,
+  };
+
+  const userData = JSON.parse(localStorage.getItem('user'));
+
+  useEffect(() => {
+    socket = io(ENDPOINT);
+    socket.emit('setup', userData);
+    socket.on('connection', () => {
+      setConnected(true);
+    });
+
+    socket.on('userJoined', (userName) => {
       setMessages((prevMessages) => [
         ...prevMessages,
-        { content: inputText, isUser: true },
+        { content: `${userName} joined the room`, isUser: false, sender: 'System' },
       ]);
-  
-      const params = {
-        botAliasId: "TSTALIASID",
-        botId: "HYFG8OJOUO",
-        localeId: "en_US",
-        sessionId: "UNIQUE_SESSION_ID",
-        text: inputText,
-      };
-  
-    //   try {
-    //     const response = await lexClient.send(command);
-    //     setMessages((prevMessages) => [
-    //       ...prevMessages,
-    //       { content: response.messages[0].content, isUser: false },
-    //     ]);
-    //   } catch (error) {
-    //     console.error("Error sending message:", error);
-    //   }
-  
-      setInputText("");
+    });
+
+    socket.on('messageReceived', (messageData) => {
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { content: messageData.text, isUser: false, sender: messageData.name },
+      ]);
+    });
+
+    socket.on('userLeft', (userName) => {
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { content: `${userName} left the room`, isUser: false, sender: 'System' },
+      ]);
+    });
+
+    socket.on('userList', (list) => {
+      setUserList(list);
+    });
+
+    return () => {
+      socket.disconnect();
     };
-  
-    const toggleChat = () => {
-      setIsExpanded((prevState) => !prevState);
-    };
-  
-    const handleInputChange = (event) => {
-      setInputText(event.target.value);
+  }, []);
+
+  const sendMessage = async (event) => {
+    event.preventDefault();
+
+    if (inputText.trim() === '') {
+      return;
+    }
+
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { content: inputText, isUser: true },
+    ]);
+
+    const messageData = {
+      text: inputText,
+      room: selectedChat._id, // Use the selected chat's _id as the room name
+      sender: userData.email // Add the sender's email to the message data
     };
 
-    useEffect(() => {
-        console.log(userData)
+    socket.emit('newMessage', messageData);
 
-        socket = io(ENDPOINT )
-        socket.emit('setup' , userData)
-        socket.on('connection', () =>{
-             setConnected(true)
-        })
-    
-    }, [])
-    
-  
-    return (
-      <div className={`chatbot-widget ${isExpanded ? "" : "minimized"}`}>
-        <div className="chatbot-widget__header">
-          <div className="chatbot-widget__header-text">Chat with your team</div>
-          {isExpanded ? (
-            <div onClick={toggleChat} style={{ marginTop: "-8px" }}>
-              <MinimizeIcon />
-            </div>
-          ) : (
-            <div onClick={toggleChat}>
-              <AddIcon />
-            </div>
-          )}
-        </div>
-        <div className="chatbot-widget__messages">
-          {messages.map((message, index) => (
-            <div key={index} style={{ marginBottom: "10px" }}>
-              {!message.isUser ? (
-                <div style={{ display: "flex" }}>
-                  <img src={ChatBot} alt="bot" class="img-chatbot" />
-                  <div className={`message-bot`}>{message.content}</div>
-                </div>
-              ) : (
-                <div className={`message-user`}>
-                  <span>You: </span>
-                  {message.content}
-                </div>
-              )}
-              <span></span>
-            </div>
-          ))}
-        </div>
-  
-        <form className="chatbot-widget__input" onSubmit={sendMessage}>
-          <input
-            type="text"
-            value={inputText}
-            onChange={handleInputChange}
-            placeholder="Type a message..."
-            style={{ marginRight: "10px" }}
-          />
-          <SendIcon onClick={sendMessage} type="submit" color="primary" />
-        </form>
-      </div>
-    );
+    setInputText('');
   };
-  
-  export default Msg;
-  
+
+  const toggleChat = () => {
+    setIsExpanded((prevState) => !prevState);
+    if (isExpanded === false) {
+      socket.emit('joinChat', selectedChat._id);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  return (
+    <div className={`chatbot-widget ${isExpanded ? '' : 'minimized'}`}>
+      <div className="chatbot-widget__header">
+        <div className="chatbot-widget__header-text">Chat with your team</div>
+        {isExpanded ? (
+          <div onClick={toggleChat} style={{ marginTop: "-8px" }}>
+            <MinimizeIcon />
+          </div>
+        ) : (
+          <div onClick={toggleChat}>
+            <AddIcon />
+          </div>
+        )}
+      </div>
+      <div className="chatbot-widget__messages">
+        {messages.map((message, index) => (
+          <div key={index} style={{ marginBottom: '10px' }}>
+            {!message.isUser ? (
+              <div style={{ display: 'flex' }}>
+                <div className="message-bot">{message.sender}: {message.content}</div>
+              </div>
+            ) : (
+              <div className="message-user">
+                <span>You: </span>
+                {message.content}
+              </div>
+            )}
+            <span></span>
+          </div>
+        ))}
+      </div>
+
+      <form className="chatbot-widget__input" onSubmit={sendMessage}>
+        <input
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder="Type a message..."
+          style={{ marginRight: '10px' }}
+        />
+       <SendIcon onClick={sendMessage} type="submit" color="primary" />
+      </form>
+      <div>
+        <h2>User List:</h2>
+        <ul>
+          {userList.map((user, index) => (
+            <li key={index}>{user}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Msg;
