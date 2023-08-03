45f32fbf718454cfc26496f4714495ed90f71fb8
jaimin <jm690660@dal.ca>
2023-08-03 01:00:12 -0300

    updated redirection.

--- Front-end/src/Components/Header.js
+++ Front-end/src/Components/Header.js
@@ -82,7 +82,7 @@ function Header() {
           // <a className='header-logo' href='https://triviatitans.auth.us-east-1.amazoncognito.com/login?client_id=6hn9vmanqlt905sa1n0skc8ql6&response_type=token&scope=email+profile+aws.cognito.signin.user.admin+openid+phone&redirect_uri=http%3A%2F%2Flocalhost%3A3000' onClick={() => navigate('/logout')}>Login</a>
           <a
             className="header-logo"
-            href="https://triviatitans.auth.us-east-1.amazoncognito.com/login?client_id=6hn9vmanqlt905sa1n0skc8ql6&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=http%3A%2F%2Flocalhost%3A3000"
+            href="https://triviatitans.auth.us-east-1.amazoncognito.com/login?client_id=6hn9vmanqlt905sa1n0skc8ql6&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https%3A%2F%2Ffrontend-o5zhrppqlq-uc.a.run.app"
             onClick={() => navigate("/logout")}
           >
             Login
